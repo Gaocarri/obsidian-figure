ab241eac39e3f4363aae7b22333469c4986473b7
GaoCarri <gaocarri@163.com>
2020-05-29 23:02:27 +0800

    router

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from './logo.svg'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (<div>
+    <img src={Logo} alt='logo' />
+    <nav>
+      <Link to="/">首页</Link>
+      <Link to="/history">历史</Link>
+      <Link to="/about">关于我</Link>
+    </nav>
+  </div>)
+}
+
+export default Header
